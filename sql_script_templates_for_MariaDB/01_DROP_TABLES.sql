99fe19eb0fcc27a12f1ff03fa6d2c68484fd5b53
trevh86 <trevor.hurt86@gmail.com>
2018-03-12 10:02:25 +0200

    Started Project

---
+++ sql_script_templates_for_MariaDB/01_DROP_TABLES.sql
@@ -0,0 +1,17 @@
+-- (C) 2007-2017 - Juhani Välimäki (Toan Khuc, Thu Nghiem)
+-- (USERNAME AND PASSWORD TO LOGIN TO MYSQL)
+-- Username: a1xxxxxx  if mariadb.haaga-helia.fi
+-- Password: pppppppp
+-- (MAKE SURE YOU ALSO CHANGE TO THE a1xxxxx DATABASE IN MYSQL)
+-- --------------------------------------------------------------------
+-- DROP is the simplest option to also reset the autoincrement counters
+-- for our test data scripts.
+
+-- --------------------- 6. last ----------------------------
+DROP TABLE Deee;
+
+--  ... other tables in opposite order than creates and inserts
+-- --------------------- 1. first ---------------------------------------------
+DROP TABLE Aaaa;
+
+
