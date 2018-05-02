4dba27e3da41070411d090454501ac89c90b287f
peckpeck20 <peckpeckthebeast@gmail.com>
2018-05-02 10:37:04 +0300

    added react boiler template

---
+++ back-end/api/category.js
@@ -0,0 +1,100 @@
+// /routes/api/people.js
+
+const express = require("express");
+const router = express.Router();
+const db = require("../database");
+// Notice: No need to require body-parser here! (This is JavaScript, not
+// Java, nor React)
+
+/** proto377.haaga-helia.fi:80/api/people , with GET**/
+router.get("/", function(req, res) {
+    db
+        .select()
+        .from("Category")
+        .then((data) => {
+            console.log(data);
+            res.status(200);
+            res.send(data);
+        });
+});
+
+//proto377.haaga-helia.fi:80/api/people , with POST !!!
+router.post("/", function(req, res) {
+    // Just a start of err handling for model for you
+    /* Dont ask me why this works */
+    // TODO: fix sql tables as newer entries not implementing foreign key constraints
+    if (
+        req.body.title &&
+        req.body.budgetLimit &&
+        (req.body.isActive === 0 || req.body.isActive === 1)
+    ) {
+        // Front-end has to send a response as JSON
+        db
+            .insert(req.body)
+            .returning("*")
+            .into("Category")
+            .then((data) => {
+                console.log(data);
+                res.status(200);
+                res.send(data);
+            })
+            .catch((error) => {
+                res.status(409); // Just a start of err handling for model for you
+                console.error(error);
+                res.end(JSON.stringify({ error: "catastrophy" }));
+            });
+    } else {
+        res.status(400); // Just a start of err handling for model for yo
+        res.end(JSON.stringify({ error: "horror" }));
+    }
+});
+
+//proto377.haaga-helia.fi:80/api/people/multiInsert , with MULTI-JSON POST !!!
+router.post("/multiInsert/", function(req, res) {
+    // Just a start of err handling for model for you
+
+    // Front-end has to send a person as JSON
+    // [{"firstName":"Joe","lastName":"Doe"},{"firstName":"Joe","lastName":"Doe"}]
+    // in request body
+    db
+        .insert(req.body)
+        .returning("*")
+        .into("Category")
+        .then((data) => {
+            console.log(data);
+            res.status(200);
+            res.send(data); // [17] if that was the auto_incement id
+            // or [17,18,19] if multiple inserts were done. valid json.org
+        })
+        .catch((error) => {
+            res.status(409); // Just a start of err handling for model for you
+            console.error(error);
+            res.end(JSON.stringify({ error: "catastrophy" }));
+        });
+});
+
+router.post("/delete/", function(req, res) {
+    let row = "title";
+    if (req.body.title) {
+        db
+            .del()
+            .from("Category")
+            .where(row, req.body.title)
+            .then((data) => {
+                console.log(data);
+                // res.send is apparently deprecated, use res.sendStatus
+                res.sendStatus(200);
+                res.sendStatus(data);
+            })
+            .catch((error) => {
+                res.sendStatus(409);
+                console.error(error);
+                res.end(JSON.stringify({ error: "horror" }));
+            });
+    } else {
+        res.sendStatus(400);
+        res.end(JSON.stringify({ error: "horror" }));
+    }
+});
+
+module.exports = router;
---
+++ back-end/index.js
@@ -0,0 +1,31 @@
+/* As this is a project for a school assignment, comments may be a bit verbose as I make sure I remember what everything is doing after I've completed it. Sorry! */
+// /index.js
+const express = require('express');
+const app = express();
+
+// Do you understand why utilizing body parser here will make it usable
+// in routes/api/people.js ?
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
+
+const apiRoute = require("./api"); // means index.js in there!!!
+
+app.use("/api", apiRoute);
+
+// Just for fast testing if something breaks down, and
+// wanna see at least that Node.js server is runnign
+app.get("/", function(req, res) {
+	res.send("Hello World of Knex Phase 2 - POST too!");
+	console.log("Things seem to be working!");
+});
+
+app.listen("8000");
