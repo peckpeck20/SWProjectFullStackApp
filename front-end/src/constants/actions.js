2dc2a7d08b98c153379f06c1adbc6054485e2618
trevh86 <trevor.hurt86@gmail.com>
2018-05-02 12:36:14 +0300

    Adding dispatchers

--- front-end/src/constants/actions.js
+++ front-end/src/constants/actions.js
@@ -1,8 +1,42 @@
 //actions for redux
+import axios from "axios";
 
-export const CREATE_IDEA = "CREATE_IDEA";
-export const GET_IDEA = "GET_IDEA";
-export const GET_ALL_IDEAS = "GET_ALL_IDEAS";
-export const UPDATE_IDEA = "UPDATE_IDEA";
-export const DELETE_IDEA = "DELETE_IDEA";
-export const GET_ALL_COMMENTS = "GET_ALL_COMMENTS";
+export const GET_ALL_IDEAS = "get_all_ideas";
+export const GET_IDEA = "idea_get";
+export const GET_ALL_COMMENTS = "get_all_comments";
+export const IDEA_CREATE = "idea_create";
+export const IDEA_UPDATE = "idea_update";
+export const IDEA_DELETE = "idea_delete";
+
+const ROOT_URL = 'localhost:8000';
+
+function receiveData(request, json) {
+  return {
+    type: request,
+    payload: json
+  }
+}
+
+export function getAllIdeas(dispatch) {
+  return axios.get(`${ROOT_URL}`).then((response) => dispatch(receiveData(GET_ALL_IDEAS, response.data)));
+}
+
+export function getIdea(id, dispatch) {
+  return axios.get(`${ROOT_URL}/${id}`).then((response) => dispatch(receiveData(GET_IDEA, response.data)));
+}
+
+export function getAllComments(id, dispatch) {
+  return axios.get(`${ROOT_URL}/${id}/comments`).then((response) => dispatch(receiveData(GET_ALL_COMMENTS, response.data)));
+}
+
+export function ideaCreate(value, dispatch) {
+  return axios.post(`${ROOT_URL}`, value).then((response) => dispatch(receiveData(IDEA_CREATE, response.data)));
+}
+
+export function ideaUpdate(id, value, dispatch) {
+  return axios.put(`${ROOT_URL}/${id}`, value).then((response) => dispatch(receiveData(IDEA_UPDATE, response.data)));
+}
+
+export function ideaDelete(id, dispatch) {
+  return axios.delete(`${ROOT_URL}/${id}`).then((response) => dispatch(receiveData(IDEA_DELETE, response.data)));
+}
