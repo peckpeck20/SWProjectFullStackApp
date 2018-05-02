a3e79f5f2fda2b07cc4625c1d0484462fabeeafd
Alex Jacobs <a1603181@myy.haaga-helia.fi>
2018-05-02 11:09:55 +0300

    Fixing duplicate problem and styling changes

--- back-end/api/index.js
+++ back-end/api/index.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const categoryRoute = require('./category');
-const memberRoute = require('./member');
-const commentRoute = require('./comment');
-const memberIdeaRoute = require('./memberidea');
-const ideaRoute = require('./idea');
+const categoryRoute = require("./category");
+const memberRoute = require("./member");
+const commentRoute = require("./comment");
+const memberIdeaRoute = require("./memberidea");
+const ideaRoute = require("./idea");
 /* Add /pets path to API */
 //const petsRoute = require('./pets');
 
 /* Same URL as in people.js */
-router.use('/category', categoryRoute);
-router.use('/member', memberRoute);
-router.use('/comment', commentRoute)
-router.use('/memberidea', memberIdeaRoute);
-router.use('/idea', ideaRoute);
+router.use("/category", categoryRoute);
+router.use("/member", memberRoute);
+router.use("/comment", commentRoute);
+router.use("/memberidea", memberIdeaRoute);
+router.use("/idea", ideaRoute);
 /* My code here */
 //router.use('/pets', petsRoute);
 
