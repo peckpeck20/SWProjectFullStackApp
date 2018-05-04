0ce66bcbdbb845ab68eefdcec68a675fa0ab0889
trevh86 <trevor.hurt86@gmail.com>
2018-05-04 15:51:01 +0300

    Make sure to add descriptive commit messages

--- MariaDB-scripts/CREATE.sql
+++ MariaDB-scripts/CREATE.sql
@@ -32,7 +32,7 @@ CREATE TABLE Idea
     
     CONSTRAINT fk_idea_category_categoryid FOREIGN KEY (categoryId)
         REFERENCES Category(id)
-        ON DELETE NO ACTION
+        ON DELETE CASCADE
 );
 
 ALTER TABLE Idea AUTO_INCREMENT = 501;   
@@ -65,7 +65,7 @@ CREATE TABLE Comment
     ON DELETE CASCADE,
     CONSTRAINT fk_Comment_Idea FOREIGN KEY (ideaId)
     REFERENCES Idea(id)
-    ON DELETE NO ACTION
+    ON DELETE CASCADE
 );
 -- ALTER TABLE Comment AUTO_INCREMENT = 1001;  
 
@@ -78,10 +78,10 @@ CREATE TABLE MemberIdea
     CONSTRAINT pk_MemberIdea PRIMARY KEY(memberId,ideaId),
     CONSTRAINT fk_MemberIdea_Member FOREIGN KEY(memberId)
     REFERENCES Member(id)
-    ON DELETE NO ACTION,
+    ON DELETE CASCADE,
     CONSTRAINT fk_MemberIdea_Idea FOREIGN KEY(ideaId)
     REFERENCES Idea(id)
-    ON DELETE NO ACTION
+    ON DELETE CASCADE
 );
 
 -- ALTER TABLE MemberIdea AUTO_INCREMENT = 1501; 
---
+++ MariaDB-scripts/RESET.sql
@@ -0,0 +1,4 @@
+source MariaDBTemplate/DROP.sql;
+source MariaDBTemplate/CREATE.sql;
+source MariaDBTemplate/INSERT.sql;
+source MariaDBTemplate/SELECT.sql;
