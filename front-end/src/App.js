349e907bfc148651ce6d66d57c44e00ecf0cf888
peckpeck20 <peckpeckthebeast@gmail.com>
2018-05-04 18:27:28 +0300

    added material ui

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import logo from "./logo.svg";
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import {BrowserRouter, Link, Switch, Route} from "react-router-dom";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import "./App.css";
 import IdeaIndex from "./components/idea_index";
 import IdeaNew from "./components/IdeaNew";
@@ -11,22 +12,24 @@ import reducers from "./reducers/index"
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="background">
-          <div className="content-wrapper">
-            <Link to="/">Home</Link>{' '}
-            <Link to="/ideas/new">New Idea</Link>{' '}
-            <Link to="/ideas/:id">Ideas by ID</Link>{' '}
-            <Link to="/edit">Edit</Link>{' '}
-            <Switch>
-              <Route exact path="/" component={IdeaIndex} />
-              <Route path="/ideas/new" component={IdeaNew} />
-              <Route path="/ideas/:id" component={IdeaShow} />
-              <Route path="/edit" component={IdeaEdit} />
-            </Switch>
+      <MuiThemeProvider>
+        <BrowserRouter>
+          <div className="background">
+            <div className="content-wrapper">
+              <Link to="/">Home</Link>
+              <Link to="/ideas/new">New Idea</Link>
+              <Link to="/ideas/:id">Ideas by ID</Link>
+              <Link to="/edit">Edit</Link>
+              <Switch>
+                <Route exact path="/" component={IdeaIndex}/>
+                <Route path="/ideas/new" component={IdeaNew}/>
+                <Route path="/ideas/:id" component={IdeaShow}/>
+                <Route path="/edit" component={IdeaEdit}/>
+              </Switch>
+            </div>
           </div>
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </MuiThemeProvider>
     );
   }
 }
--- front-end/src/reducers/index.js
+++ front-end/src/reducers/index.js
@@ -1,9 +1,7 @@
-import { combineReducers } from "redux";
-import { reducer as formReducer } from "redux-form";
+import {combineReducers} from "redux";
+import {reducer as formReducer} from "redux-form";
 import reducerIdea from "./reducerIdea";
 
-const rootReducer = combineReducers({
-  idea: reducerIdea,
-});
+const rootReducer = combineReducers({idea: reducerIdea, IdeaNewForm: formReducer});
 
 export default rootReducer;
