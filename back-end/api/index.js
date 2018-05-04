02837cea7f7c83082170c17da1c8b18b38ac80cb
Alex Jacobs <a1603181@myy.haaga-helia.fi>
2018-05-04 18:01:51 +0300

    Fixes to backend, frontend changes

--- back-end/api/index.js
+++ back-end/api/index.js
@@ -5,16 +5,11 @@ const memberRoute = require("./member");
 const commentRoute = require("./comment");
 const memberIdeaRoute = require("./memberidea");
 const ideaRoute = require("./idea");
-/* Add /pets path to API */
-//const petsRoute = require('./pets');
 
-/* Same URL as in people.js */
 router.use("/category", categoryRoute);
 router.use("/member", memberRoute);
 router.use("/comment", commentRoute);
 router.use("/memberidea", memberIdeaRoute);
 router.use("/idea", ideaRoute);
-/* My code here */
-//router.use('/pets', petsRoute);
 
 module.exports = router;
--- back-end/database/index.js
+++ back-end/database/index.js
@@ -1,12 +1,12 @@
 /* Define database connection here */
 const knex = require("knex")({
-	client: "mysql",
-	connection: {
-		host: "localhost",
-		database: "a1603181",
-		user: "a1603181",
-		password: "jiHI2i59z"
-	}
+  client: "mysql",
+  connection: {
+    host: "localhost",
+    database: "a1603181",
+    user: "a1603181",
+    password: "jiHI2i59z"
+  }
 });
 
 module.exports = knex;
