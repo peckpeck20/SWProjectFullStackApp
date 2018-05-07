840db5f95a42653be06f029ef0f3bb50c7793e9a
trevh86 <trevor.hurt86@gmail.com>
2018-05-07 16:48:07 +0300

    fixing reducer and store problem on redux form

--- front-end/src/reducers/index.js
+++ front-end/src/reducers/index.js
@@ -2,6 +2,6 @@ import {combineReducers} from "redux";
 import {reducer as formReducer} from "redux-form";
 import reducerIdea from "./reducerIdea";
 
-const rootReducer = combineReducers({idea: reducerIdea, IdeaNewForm: formReducer});
+const rootReducer = combineReducers({idea: reducerIdea, form: formReducer});
 
 export default rootReducer;
