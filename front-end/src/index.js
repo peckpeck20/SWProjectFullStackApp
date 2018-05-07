085581bd62466124398e9d89eda0e7ce6ef74642
Alex Jacobs <a1603181@myy.haaga-helia.fi>
2018-05-07 16:50:34 +0300

    something

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {Provider} from "react-redux";
-import {createStore} from "redux";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 // import promise from "redux-promise";
 import rootReducer from "./reducers/index";
 
@@ -15,8 +15,8 @@ const store = createStore(rootReducer, {
 
 ReactDOM.render(
   <Provider store={store}>
-  <App/>
-</Provider>, document.getElementById('root'));
+    <App />
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
 
 console.log('====================================');
